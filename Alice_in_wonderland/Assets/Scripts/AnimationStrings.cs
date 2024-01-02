3d5694331fa27e0ec5218356d058f9496a17f7b2
anhthep119 <phamtheanhgl2001@gmail.com>
2024-01-02 22:31:03 +0700

    update

---
+++ Alice_in_wonderland/Assets/Scripts/AnimationStrings.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+internal class AnimationStrings
+{
+    internal static string isMoving = "isMoving";
+    internal static string isRunning = "isRunning";
+}
