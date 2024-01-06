f7d78ac4bd95dcb11ceff3d09cdf5b5e7d2dfcee
anhthep119 <phamtheanhgl2001@gmail.com>
2024-01-06 21:29:21 +0700

    fix knight khong dung im khi tan cong, update combo attack cua player

--- Alice_in_wonderland/Assets/Scripts/Knight.cs
+++ Alice_in_wonderland/Assets/Scripts/Knight.cs
@@ -6,8 +6,9 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D),typeof(TouchingDirections), typeof(Damageable))]
 public class Knight : MonoBehaviour
 {
-    public float walkSpeed = 3f;
-    public float walkStopRate = 0.6f;
+    public float walkAcceleration = 3f;
+    public float maxSpeed = 3f;
+    public float walkStopRate = 0.05f;
     public DetectionZone attackZone;
     public DetectionZone cliffDetectionZone;
     Rigidbody2D rb;
@@ -89,7 +90,8 @@ public class Knight : MonoBehaviour
         {
             if (CanMove)
             {
-                rb.velocity = new Vector2(walkSpeed * walkDirectionVector.x, rb.velocity.y);
+                
+                rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x + (walkAcceleration * walkDirectionVector.x * Time.fixedDeltaTime), -maxSpeed, maxSpeed), rb.velocity.y);
             }
             else
             {
