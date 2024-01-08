d75063f0c3bba36b497b7a60decdbd47315c75d4
anhthep119 <phamtheanhgl2001@gmail.com>
2024-01-08 17:02:48 +0700

    update thanh mau

--- Alice_in_wonderland/Assets/Scripts/Damageable.cs
+++ Alice_in_wonderland/Assets/Scripts/Damageable.cs
@@ -7,6 +7,7 @@ public class Damageable : MonoBehaviour
 {
     public UnityEvent<int, Vector2> damageableHit;
     public UnityEvent damageableDeath;
+    public UnityEvent<int, int> healthChanged;
 
     Animator animator;
     [SerializeField]
@@ -34,6 +35,7 @@ public class Damageable : MonoBehaviour
         set
         {
             _health = value;
+            healthChanged.Invoke(_health, MaxHealth);
             if(_health <= 0) 
             {
                 IsAlive = false;
